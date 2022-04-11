6c3120c35d3a09630c6a86c1d8040217a15c521d
Snownee <i@snownee.com>
2022-04-11 16:26:43 +0800

    feat: add support for REI's left mob effects option (#2)

---
+++ src/main/java/snownee/minieffects/MiniEffects.java
@@ -0,0 +1,23 @@
+package snownee.minieffects;
+
+import me.shedaniel.rei.api.client.config.ConfigObject;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class MiniEffects implements ClientModInitializer {
+
+	public static boolean hasREI;
+
+	@Override
+	public void onInitializeClient() {
+		hasREI = FabricLoader.getInstance().isModLoaded("roughlyenoughitems");
+	}
+
+	public static boolean isLeftSide() {
+		if (hasREI) {
+			return ConfigObject.getInstance().isLeftSideMobEffects();
+		}
+		return false;
+	}
+
+}
