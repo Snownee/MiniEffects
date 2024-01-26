a94ece907b64070743d6d41ef9355818802e9340
Snownee <i@snownee.com>
2024-01-26 12:57:24 +0800

    port: 1.20.4

--- src/main/java/snownee/minieffects/mixin/jei/InventoryEffectRendererGuiHandlerMixin.java
+++ src/main/java/snownee/minieffects/mixin/jei/InventoryEffectRendererGuiHandlerMixin.java
@@ -3,17 +3,16 @@ package snownee.minieffects.mixin.jei;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import mezz.jei.library.plugins.vanilla.InventoryEffectRendererGuiHandler;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.client.renderer.Rect2i;
 import snownee.minieffects.IAreasGetter;
 
-@Mixin(targets = "mezz.jei.library.plugins.vanilla.InventoryEffectRendererGuiHandler", remap = false)
-@Pseudo
+@Mixin(value = InventoryEffectRendererGuiHandler.class, remap = false)
 public class InventoryEffectRendererGuiHandlerMixin {
 
 	@Inject(method = "getGuiExtraAreas", at = @At("HEAD"), cancellable = true, require = 0)
